e09cf3f975c66da8fdc821c9dc5b01c427b5e6bb
AlexandruGabi <constantinescu.alexandrugabi47@gmail.com>
2025-06-23 01:55:43 +0300

    Added Unity Project

---
+++ Assets/GameSrc/Scripts/CharacterScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterScript : MonoBehaviour
+{
+	//
+	Rigidbody2D rb;
+	int HP; 
+	//
+	public float maxSpeed;
+	public float jumpForce;
+	//
+	float speed;
+	float direction;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		//
+		HP = 1;
+		maxSpeed = 15;
+		jumpForce = 26;
+		//
+		speed = 0;
+		direction = 0;
+	}
+
+	private void FixedUpdate()
+	{
+		/*Input*/
+		if (Input.GetKeyDown(KeyCode.W)) Jump();
+		direction = Input.GetAxis("Horizontal");
+		Move();
+		//Fire
+		if (Input.GetKeyDown(KeyCode.Space)) Fire();
+		//Checks if it exceeds the maxSpeed or the jumpForce
+		if (rb.linearVelocity.x > maxSpeed)		
+			rb.linearVelocity = new Vector2(maxSpeed, rb.linearVelocity.y);		
+		if (rb.linearVelocity.y > jumpForce)
+			rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+	}
+
+	public void Move()
+	{
+		//Rotates the character in the direction he is moving
+		if (direction < 0) transform.rotation = new Quaternion(0, 180, 0, 0);
+		if (direction > 0) transform.rotation = new Quaternion(0, 0, 0, 0);
+		//
+		rb.linearVelocity = new Vector2(direction * maxSpeed, rb.linearVelocity.y);
+	}
+
+	void Update()
+	{
+		if (HP == 0) Kill();
+	}
+
+	void Jump()
+	{
+		if (rb.linearVelocity.y == 0)
+		{
+			rb.AddForce(new Vector2(0, jumpForce * 100));
+		}
+	}
+
+	void Fire()
+	{
+		Instantiate((GameObject)Resources.Load("Prefabs/Bullet"), rb.transform.position, transform.rotation);
+	}
+
+	public void Kill()
+	{
+		rb.linearVelocity = new Vector2(0, 0);
+		transform.position = new Vector3(0, 0, 0);
+	}
+}
---
+++ Assets/GameSrc/Scripts/ZombieScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+	int HP;
+	void Start()
+	{
+		HP = 2;
+	}
+	void Update()
+	{
+		if (HP == 0) Kill();
+	}
+	void FixedUpdate()
+	{
+		
+	}
+	public void Kill()
+	{
+		Destroy(this.gameObject);
+	}
+}
