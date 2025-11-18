c3ffa71f3faeb47873f445f1885f67b25a7c25f2
AlexandruGabi <constantinescu.alexandrugabi47@gmail.com>
2025-11-18 04:01:50 +0200

    Small update, added sound for gun

--- Assets/_Scripts/Player/PlayerCharacter.cs
+++ Assets/_Scripts/Player/PlayerCharacter.cs
@@ -2,9 +2,11 @@ using CAUnityFramework;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMB : MonoBehaviour
+public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField] private List<AudioClip> hurtSounds;
+    //Temp
+    [SerializeField] private AudioClip backgroundMusic;
 
     private Health health;
 
@@ -12,11 +14,19 @@ public class PlayerMB : MonoBehaviour
     {
         this.health = this.GetComponent<Health>();
         this.health.OnHit.AddListener(this.OnHit);
+        AudioManager.PlaySound(AudioChannel.Music, 
+            new AudioPlayRequest(
+	            clip: this.backgroundMusic,
+                volume: 0.5f,
+	            loop: true,
+	            fadeIn: true,
+	            fadeDuration: 2f));
 	}
 
     private void OnHit()
     {
         AudioClip clip = this.hurtSounds[Random.Range(0, this.hurtSounds.Count)];
         AudioSource.PlayClipAtPoint(clip, this.transform.position);
+		AudioManager.PlaySound(AudioChannel.Voice, new AudioPlayRequest(clip));
     }
 }
